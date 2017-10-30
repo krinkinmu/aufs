ad85994374cde6fdb9e86ae21855f0bb021fa969
Nandu B C <b.c.nandu17@gmail.com>
2017-10-30 20:02:13 +0530

    Direct use of new_sync_read not recommended

--- kern/file.c
+++ kern/file.c
@@ -2,7 +2,7 @@
 
 const struct file_operations aufs_file_ops = {
 	.llseek = generic_file_llseek,
-	.read = new_sync_read,
+	.read = NULL,
 	.read_iter = generic_file_read_iter,
 	.mmap = generic_file_mmap,
 	.splice_read = generic_file_splice_read
